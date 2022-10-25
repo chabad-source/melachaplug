490050127bb156a578a3aa7f9c21ba3a0bd157e7
Rebbe Pod <66928914+RebbePod@users.noreply.github.com>
2022-10-24 23:02:26 -0400

    New Update
    Changed the configuration layout

--- esphome/plugins/hdate/hdate.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-
-#include "../../../../plugins/hebrewcalender_melachaplug/hebrewcalender_melachaplug.h"
-
-namespace Hdate  {
-    hdate current_hdate;  // global varible for todays hebrew date
-} // namespace Hdate
